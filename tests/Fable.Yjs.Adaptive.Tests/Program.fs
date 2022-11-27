a869955fce5c67bef0de8bc29802ec0d23d2ad54
Nick Darvey <nickdarvey@outlook.com.au>
2022-11-27 20:46:47 +1100

    disable other tests

--- tests/Fable.Yjs.Adaptive.Tests/Program.fs
+++ tests/Fable.Yjs.Adaptive.Tests/Program.fs
@@ -6,7 +6,7 @@ open Fable.Mocha
 let main _ =
     Mocha.runTests <| testList "" [
         FSharp.Data.Adaptive.Codec.tests
-        Index.tests
-        Y.Delta.tests
-        Y.Text.tests
+        // Index.tests
+        // Y.Delta.tests
+        // Y.Text.tests
     ]
